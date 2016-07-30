b99d3d47677ec181716e62c54aac7b99209442f6
Thomas Adamcik <thomas@adamcik.no>
2016-07-31 00:09:32 +0200

    Try and document the ratelimiting

--- oauthclientbridge.py
+++ oauthclientbridge.py
@@ -60,6 +60,13 @@ def decrypt(key, token):
 
 
 def rate_limit(key):
+    """Decide if the given key should be rate limited.
+
+    Calls are allowed whenever the bucket is below capacity. Each hit fills the
+    bucket by one. Buckets drain at a configurable rate, though refill only
+    happens when the bucket gets a hit. There is a maximum bucket fill to avoid
+    callers being locket out for too long.
+    """
     now = time.time()
     key = hashlib.sha256(key).hexdigest()
 
@@ -75,8 +82,12 @@ def rate_limit(key):
 
         # TODO: add a penalty for being over cap?
         # TODO: this is probably racy.
+
+        # 1. Reduce by amount we should have refilled since last update.
         value -= float(now - updated) / app.config['OAUTH_BUCKET_REFILL_RATE']
+        # 2. Update to 0 if bucket is "full" or value + 1 to account for hit.
         value = max(0, value + 1)
+        # 3. Limit how much over you can go.
         value = min(value, app.config['OAUTH_BUCKET_MAX_HITS'])
 
         cursor.execute(  # Insert/replace the bucket we just hit.
