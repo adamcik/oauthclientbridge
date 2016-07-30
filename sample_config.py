fc288ae1edd32b6ad8efab7ec4cdd09fdf6cf781
Thomas Adamcik <thomas@adamcik.no>
2016-07-30 23:43:59 +0200

    Improve sample config documentation a bit

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,17 @@
-# Secret key used for encrypting session cookies used in initial OAuth flow:
+# Secret key used for encrypting session cookies used in initial OAuth flow
+#
+# Run the following code in a Python shell and then copy the entire value as is
+# into this file for a good secret key.
+#
+#   >>> import os
+#   >>> os.urandom(24)
+#   '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
+#
+# Also make sure you have secure access to this file _and_ the directory it's
+# in to keep it from leaking via .pyc files.
 SECRET_KEY = 'MUST-BE-REPLACED-WITH-A-GOOD-RANDOM-VALUE'
 
-# Sqlite3 database to store tokens and rate limit information in:
+# SQLite3 database to store tokens and rate limit information in:
 OAUTH_DATABASE = 'oauth.db'
 
 # Client ID and secret provided by upstream OAuth provider:
@@ -9,7 +19,7 @@ OAUTH_CLIENT_ID = 'some-client-id'
 OAUTH_CLIENT_SECRET = 'some-client-secret'
 
 # Upstream authorization URI to redirect users to:
-OAUTH_AUTHORIZATION_URI= 'https://example.com/authorize'
+OAUTH_AUTHORIZATION_URI = 'https://example.com/authorize'
 
 # Upstream token and refresh URIs, this will often be the same endpoint:
 OAUTH_TOKEN_URI = 'https://example.com/token'
@@ -24,6 +34,10 @@ OAUTH_REDIRECT_URI = 'http://localhost:5000/callback'
 
 # Jinja2 template to use for the callback page. Possible context values are:
 #  error, client_id, client_secret
+#
+# Should be setup to give the client_id and client_secret to the user. Either
+# directly or passing the value back to the parent window if this is being run
+# in a pop-up window.
 OAUTH_CALLBACK_TEMPLATE = """
 {% if error %}
   {{ error }}
@@ -33,6 +47,10 @@ OAUTH_CALLBACK_TEMPLATE = """
     Client Secret: <input name="client_secret" value="{{ client_secret }}" />
     Grant type: <input name="grant_type" value="client_credentials" />
     <button>Fetch token</button>
+</form>
+<form action="revoke" method="POST">
+    Client ID: <input name="client_id" value="{{ client_id }}" />
+    <button>Revoke token</button>
 </form>
 {% endif %}
 """
