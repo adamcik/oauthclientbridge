ee9b751c64e1d1a20163cf594268c9dd44e6e88a
Thomas Adamcik <thomas@adamcik.no>
2016-08-02 22:56:26 +0200

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='OAuth-Client-Bridge',
-    version='0.1.0',
+    version='1.0.0',
     url='https://github.com/adamcik/oauthclientbridge',
     license='Apache License, Version 2.0',
     author='Thomas Adamcik',
