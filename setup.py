978cde178eb5987994cf0485298d2c2a9e9a09cb
Thomas Adamcik <thomas@adamcik.no>
2019-02-02 15:20:52 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='OAuth-Client-Bridge',
-    version='1.0.1',
+    version='1.1.0',
     url='https://github.com/adamcik/oauthclientbridge',
     license='Apache License, Version 2.0',
     author='Thomas Adamcik',
