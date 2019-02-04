892d84a8ac016e4cf429b79fa3e71b5e5dce3cf6
Thomas Adamcik <thomas@adamcik.no>
2019-02-04 05:59:53 +0100

    Add TODO to schema for exploring without rowids

--- schema.sql
+++ schema.sql
@@ -5,10 +5,4 @@ create table tokens(
   client_id text primary key,
   token blob
 );
-
-drop table if exists buckets;
-create table buckets (
-  key text primary key,
-  value real not null default 0,
-  updated int not null
-);
+-- TODO: Consider WITHOUT ROWID;?
