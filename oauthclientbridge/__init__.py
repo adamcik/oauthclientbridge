906cc6ae36df13fca913521bf3d2d4286820164f
Thomas Adamcik <thomas@adamcik.no>
2019-02-06 09:03:05 +0100

    Ignore OAUTH_SETTINGS not being set for tests etc

--- oauthclientbridge/__init__.py
+++ oauthclientbridge/__init__.py
@@ -8,7 +8,7 @@ __version__ = '1.0.1'
 
 app = Flask(__name__)
 app.config.from_object('oauthclientbridge.default_settings')
-app.config.from_envvar('OAUTH_SETTINGS')
+app.config.from_envvar('OAUTH_SETTINGS', silent=True)
 
 if app.config['OAUTH_NUM_PROXIES']:
     app.wsgi_app = ProxyFix(app.wsgi_app, app.config['OAUTH_NUM_PROXIES'])
