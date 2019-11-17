7dd4c02937c4794b7a11b21a448e893e3a207b52
Thomas Adamcik <thomas@adamcik.no>
2019-11-17 10:23:05 +0100

    Modernize setup based on Mopidy's extension cookiecutter

--- setup.py
+++ setup.py
@@ -1,28 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='OAuth-Client-Bridge',
-    version='1.3.0',
-    url='https://github.com/adamcik/oauthclientbridge',
-    license='Apache License, Version 2.0',
-    author='Thomas Adamcik',
-    author_email='thomas@adamcik.no',
-    description='Bridge OAuth2 Authorization Code Grants to Clients Grants.',
-    long_description=open('README.rst').read(),
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'cryptography',
-        'Flask>=0.11',
-        'pyprometheus',
-        'requests',
-    ],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ],
-)
+setup()
