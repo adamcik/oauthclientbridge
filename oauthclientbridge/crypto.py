8fa55f00d4a1718102df66a17bdf6cb775337c5d
Thomas Adamcik <thomas@adamcik.no>
2019-12-08 08:44:38 +0100

    Type annotate crypto module

--- oauthclientbridge/crypto.py
+++ oauthclientbridge/crypto.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, Text, Union  # noqa: F401
+
 import json
 
 from cryptography import fernet
@@ -5,18 +7,18 @@ from cryptography import fernet
 InvalidToken = fernet.InvalidToken
 
 
-def generate_key():
+def generate_key():  # type: () -> Text
     """Cryptographically safe key that is human readable."""
     return fernet.Fernet.generate_key().decode('ascii')
 
 
-def dumps(key, data):
+def dumps(key, data):  # type: (Text, Dict[str, Union[str, int, float]]) -> bytes
     """Calls json.dumps on data and encrypts the result with given key."""
     f = fernet.Fernet(key.encode('ascii'))
     return f.encrypt(json.dumps(data).encode('utf-8'))
 
 
-def loads(key, token):
+def loads(key, token):  # type: (Text, bytes) -> Dict[str, Union[str, int, float]]
     """Decrypts and verifies token with given key and calls json.loads."""
     f = fernet.Fernet(key.encode('ascii'))
     return json.loads(f.decrypt(token).decode('utf-8'))
