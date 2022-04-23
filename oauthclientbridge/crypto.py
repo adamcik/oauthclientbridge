c04037e07cce963a0bd6bc2812ea5161146ea7e5
Thomas Adamcik <thomas@adamcik.no>
2022-04-23 11:10:26 +0200

    Update crypto module typing

--- oauthclientbridge/crypto.py
+++ oauthclientbridge/crypto.py
@@ -1,27 +1,24 @@
 import json
-import typing
 
 from cryptography import fernet
 
-if typing.TYPE_CHECKING:
-    from typing import Any, Dict, Text  # noqa: F401
-
+from typing import Any
 
 InvalidToken = fernet.InvalidToken
 
 
-def generate_key():  # type: () -> Text
+def generate_key() -> str:
     """Cryptographically safe key that is human readable."""
     return fernet.Fernet.generate_key().decode("ascii")
 
 
-def dumps(key, data):  # type: (Text, Dict[Text, Any]) -> bytes
+def dumps(key: str, data: dict[str, Any]) -> bytes:
     """Calls json.dumps on data and encrypts the result with given key."""
     f = fernet.Fernet(key.encode("ascii"))
     return f.encrypt(json.dumps(data).encode("utf-8"))
 
 
-def loads(key, token):  # type: (Text, bytes) -> Dict[Text, Any]
+def loads(key: str, token: bytes) -> dict[str, Any]:
     """Decrypts and verifies token with given key and calls json.loads."""
     f = fernet.Fernet(key.encode("ascii"))
     return json.loads(f.decrypt(token).decode("utf-8"))
