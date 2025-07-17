974545e877dd2110639b335137bcbbf573472aca
Thomas Adamcik <thomas@adamcik.no>
2025-07-17 21:47:23 +0200

    style: Fix ruff checks that did not pass

--- oauthclientbridge/__init__.py
+++ oauthclientbridge/__init__.py
@@ -9,12 +9,15 @@ __version__ = importlib.metadata.version("oauthclientbridge")
 
 app = Flask(__name__)
 app.config.from_object("oauthclientbridge.default_settings")
-app.config.from_envvar("OAUTH_SETTINGS", silent=True)
+_ = app.config.from_envvar("OAUTH_SETTINGS", silent=True)
 
 if app.config["OAUTH_NUM_PROXIES"]:
-    wrapper = ProxyFix(app.wsgi_app, app.config["OAUTH_NUM_PROXIES"])
-    app.wsgi_app = wrapper  # type: ignore
+    wrapper = ProxyFix(
+        app.wsgi_app,
+        x_for=app.config["OAUTH_NUM_PROXIES"],
+    )
+    app.wsgi_app = wrapper
 
-import oauthclientbridge.cli
-import oauthclientbridge.logging
-import oauthclientbridge.views
+import oauthclientbridge.cli  # noqa
+import oauthclientbridge.logging  # noqa
+import oauthclientbridge.views  # noqa
