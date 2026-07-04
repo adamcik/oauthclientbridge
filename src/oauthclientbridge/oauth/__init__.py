ab95b2b76347791c7454b3482971fa6ec81b68d7
Thomas Adamcik <thomas@adamcik.no>
2026-07-04 21:13:35 +0000

    docs: capture import and test preferences

--- src/oauthclientbridge/oauth/__init__.py
+++ src/oauthclientbridge/oauth/__init__.py
@@ -10,21 +10,15 @@ from .core import (
 from .outcome import (
     AUTHORIZATION_ERRORS,
     TOKEN_ERRORS,
-    OAuthResponse,
-    TokenEndpointOutcome,
     normalize_error,
     token_endpoint_outcome,
     validate_token,
 )
-from .retry import RetryReason
 
 __all__ = [
     "Error",
     "AUTHORIZATION_ERRORS",
-    "OAuthResponse",
-    "RetryReason",
     "TOKEN_ERRORS",
-    "TokenEndpointOutcome",
     "error_handler",
     "fallback_error_handler",
     "fetch",
